03a22c24733f8d1bfada99d4ef8c75f302118a6f
Horia Coman <horia141@gmail.com>
2017-11-11 09:59:12 +0200

    Initial commit

---
+++ diff-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+git diff
+git diff origin/master master
+
+cd ..
+
+cd common-js
+git diff
+git diff origin/master master
+cd ..
+
+cd common-server-js
+git diff
+git diff origin/master master
+cd ..
+
+cd identity
+git diff
+git diff origin/master master
+cd ..
+
+cd identity-sdk-js
+git diff
+git diff origin/master master
+cd ..
+
+cd infra
---
+++ pull-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git pull
+
+cd ..
+
+cd common-js
+git pull
+cd ..
+
+cd common-server-js
+git pull
+cd ..
+
+cd identity
+git pull
+cd ..
+
+cd identity-sdk-js
+git pull
+cd ..
+
+cd infra
